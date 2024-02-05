fa12631e6deaed4c8c1c54f9f44f118f40fb776a
ABRARgomaa <abrargomaa111@gmail.com>
2024-02-06 01:12:03 +0200

    up

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,22 +1,12 @@
-#!/bin/bash
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
-# MySQL credentials
-MYSQL_USER="root"
-MYSQL_PASSWORD="your_root_password"
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
-# Database and user information
-DB_NAME="hbnb_dev_db"
-DB_USER="hbnb_dev"
-DB_PASSWORD="hbnb_dev_pwd"
+-- Grant privileges to the user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
-# Commands to execute
-SQL_QUERY1="CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-SQL_QUERY2="CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
-SQL_QUERY3="GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
-SQL_QUERY4="GRANT SELECT ON performance_schema.* TO '$DB_USER'@'localhost';"
-
-# Execute SQL queries
-mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "$SQL_QUERY1"
-mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "$SQL_QUERY2"
-mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "$SQL_QUERY3"
-mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "$SQL_QUERY4"
+-- Flush privileges
+FLUSH PRIVILEGES;
