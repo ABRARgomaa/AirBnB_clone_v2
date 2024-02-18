a72c5071e9b6dac32086a574630ea2e49002e403
ABRARgomaa <abrargomaa111@gmail.com>
2024-02-18 02:00:16 +0200

    up

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,10 +3,9 @@
  Hello Flask!
 """
 from flask import Flask, render_template
-from models import storage, state
+from models import storage, State  # Import State from models module
 
 web_app = Flask(__name__)
-
 
 @web_app.route("/states_list", strict_slashes=False)
 def states_list():
@@ -17,11 +16,9 @@ def states_list():
     sorted_states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=sorted_states)
 
-
 @web_app.teardown_appcontext
 def close_storage(error):
     storage.close()
-
 
 if __name__ == "__main__":
     web_app.run(host='0.0.0.0', port=5000)
